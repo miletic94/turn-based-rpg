b77d3db121f78519958d3cd892de5b750651ef4d
miletic94 <mileticnemanja.lab@gmaol.com>
2026-06-12 16:33:07 +0200

    Move Tooltip position is better positioned. Not going out of visibility

--- Assets/Core/Game/GamePlay/Scripts/Presentation/Tooltip/MoveDescriptionTooltipView.cs
+++ Assets/Core/Game/GamePlay/Scripts/Presentation/Tooltip/MoveDescriptionTooltipView.cs
@@ -7,7 +7,6 @@ public class MoveDescriptionTooltipView :
 {
     [SerializeField] TMP_Text _tooltipTextContainer;
     [SerializeField] RectTransform _rectTransform;
-    [SerializeField] private Vector2 _offset;
 
 
     public void Show(MoveDescriptionTooltipMessage data)
@@ -29,9 +28,23 @@ public class MoveDescriptionTooltipView :
         Vector3[] corners = new Vector3[4];
         anchor.GetWorldCorners(corners);
 
-        Vector3 position = corners[2];
+        var screenPos = RectTransformUtility.WorldToScreenPoint(null, corners[2]);
+        bool isUpperHalf = screenPos.y > UnityEngine.Screen.height * 0.5f;
 
-        _rectTransform.position = position + (Vector3)_offset;
+        if (isUpperHalf)
+        {
+            var bottomCenter = (corners[0] + corners[3]) * 0.5f;
+
+            _rectTransform.pivot = new Vector2(0.5f, 1f);
+            _rectTransform.position = bottomCenter;
+        }
+        else
+        {
+            var topCenter = (corners[1] + corners[2]) * 0.5f;
+
+            _rectTransform.pivot = new Vector2(0.5f, 0f);
+            _rectTransform.position = topCenter;
+        }
     }
 
     public void Hide()
